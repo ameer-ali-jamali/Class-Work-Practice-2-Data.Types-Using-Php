1ea7ad130347e747f50677658374e815490622f4
Ameer-Ali-Jamali <ameer.ali.jamali886@gmail.com>
2022-10-08 14:54:33 +0500

    if else condition

--- index.php
+++ index.php
@@ -1,43 +1,86 @@
 <?php
 
 
-// Print Functions;
+// // Print Functions;
 
-//if else conditions...
+// //if else conditions...
 
-$a=2;
-$b=2;
+// $a=2;
+// $b=2;
 
-if(2==2){
-echo"equal";
+// if(2==2){
+// echo"equal";
+// }
+
+// // 2nd if condition......
+
+// if($a==$b):
+// echo $a==$b;
+// endif;
+
+// // New oprator whuch can give 3 values 1 or 0 or minus..
+// //If with else condition....
+
+// if(2<=>2){
+//     echo"equal";
+// }
+// else{
+//     echo "not equal";
+// }
+
+// //if else with logicall oprators.......
+
+// if(2==2 or 3==3){
+//     echo"equal";
+//     }
+// if(2==2 and 3==3){
+//     echo"equal";
+//     }
+// else{
+//     echo"not equal";
+// }
+
+$a=10;
+$b=9;
+
+if($a%2==0){
+    echo$a."<h1>This is Even Number</h1>";
 }
 
-// 2nd if condition......
+if($b%2!=0){
+    echo$b."<h1>This Is Odd Number</h1>";
+    }
+else{
+echo"error";
+}
 
-if($a==$b):
-echo $a==$b;
-endif;
+$c=-2;
 
-// New oprator whuch can give 3 values 1 or 0 or minus..
-//If with else condition....
-
-if(2<=>2){
-    echo"equal";
+if($c<0){
+    echo $c."<h1>This Is Negative </h1>";
+}
+elseif($c>0){
+    echo $c."<h1>This Is Positve </h1>";
 }
 else{
-    echo "not equal";
+    echo"error";
 }
 
-//if else with logicall oprators.......
+$d="admin";
+$e="123";
 
-if(2==2 or 3==3){
-    echo"equal";
-    }
-if(2==2 and 3==3){
-    echo"equal";
-    }
+if($d=="admin" and $e=="123"){
+    echo $d."<h1> Wellcome Admin </h1>";
+}
+elseif($d!="admin" and $e!="123"){
+    echo $d."<h1>Sorry Try Again </h1>";
+}
 else{
-    echo"not equal";
+    echo"error";
 }
+
+$iphone=200000;
+$android=100000;
+$tech=40000;
 
 ?>
