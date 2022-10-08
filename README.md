2f9f2d5cbda3056f86aa6411a3bb5d4a820a7f51
Ameer-Ali-Jamali <ameer.ali.jamali886@gmail.com>
2022-10-08 13:49:31 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Class-Work-Practice-1-Using-Php
+
+today is our first class on php we learning php echo print and more
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+
+// Print Functions;
+
+//if else conditions...
+
+$a=2;
+$b=2;
+
+if(2==2){
+echo"equal";
+}
+
+// 2nd if condition......
+
+if($a==$b):
+echo $a==$b;
+endif;
+
+// New oprator whuch can give 3 values 1 or 0 or minus..
+//If with else condition....
+
+if(2<=>2){
+    echo"equal";
+}
+else{
+    echo "not equal";
+}
+
+?>
