9b78eacfcc5632d201f272d2065c37f91108bff5
Ameer-Ali-Jamali <ameer.ali.jamali886@gmail.com>
2022-10-10 17:01:42 +0500

    practice

--- index.php
+++ index.php
@@ -1,86 +1,145 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1 {
+            color: darkblue;
+            background-color: blanchedalmond;
+            font-size: large;
+            font-weight: bold;
+            text-align: center;
+
+        }
+
+        body {
+            text-align: center;
+            font-size: large;
+            font-weight: 600;
+        }
+    </style>
+
+</head>
+
+<body>
+
+    <?php
+
+    // // // Print Functions;
+    // // //if else conditions...
+    // $a = 2;
+    // $b = 2;
+
+    // if (2 == 2) {
+    //     echo $a . "==" . $b . "<h1>This is Equal</h1>";
+    // }
+
+    // // 2nd if condition......
+
+    // if ($a == $b) :
+    //     echo $a == $b;
+    // endif;
+
+    // // New oprator whuch can give 3 values 1 or 0 or minus..
+    // //If with else condition....
+
+    // if (2 <=> 2) {
+    //     echo "equal";
+    // } else {
+    //     echo "not equal";
+    // }
+
+    // //if else with logicall oprators.......
+
+    // if (2 == 2 or 3 == 3) {
+    //     echo "equal";
+    // }
+    // if (2 == 2 and 3 == 3) {
+    //     echo "equal";
+    // } else {
+    //     echo "not equal";
+    // }
 
 
-// // Print Functions;
+    // //Even and odd numbers..
 
-// //if else conditions...
+    // $a = 10;
+    // $b = 9;
 
-// $a=2;
-// $b=2;
+    // if ($a % 2 == 0) {
+    //     echo $a . "<h1>This is Even Number</h1>";
+    // }
 
-// if(2==2){
-// echo"equal";
-// }
+    // if ($b % 2 != 0) {
+    //     echo $b . "<h1>This Is Odd Number</h1>";
+    // } else {
+    //     echo "error";
+    // }
 
-// // 2nd if condition......
+    // //Negative and posative numbers..
 
-// if($a==$b):
-// echo $a==$b;
-// endif;
+    // $c = -2;
 
-// // New oprator whuch can give 3 values 1 or 0 or minus..
-// //If with else condition....
+    // if ($c < 0) {
+    //     echo $c . "<h1>This Is Negative </h1>";
+    // } elseif ($c > 0) {
+    //     echo $c . "<h1>This Is Positve </h1>";
+    // } else {
+    //     echo "error";
+    // }
 
-// if(2<=>2){
-//     echo"equal";
-// }
-// else{
-//     echo "not equal";
-// }
+    // //Admin login 
 
-// //if else with logicall oprators.......
+    // $d = "admin";
+    // $e = "123";
 
-// if(2==2 or 3==3){
-//     echo"equal";
-//     }
-// if(2==2 and 3==3){
-//     echo"equal";
-//     }
-// else{
-//     echo"not equal";
-// }
+    // if ($d == "admin" and $e == "123") {
+    //     echo $d . "<h1> Wellcome Admin </h1>";
+    // } elseif ($d != "admin" and $e != "123") {
+    //     echo $d . "<h1>Sorry Try Again </h1>";
+    // } else {
+    //     echo "error";
+    // }
 
-$a=10;
-$b=9;
+    //Percentage condition function calculate percentage of each brand..
 
-if($a%2==0){
-    echo$a."<h1>This is Even Number</h1>";
-}
 
-if($b%2!=0){
-    echo$b."<h1>This Is Odd Number</h1>";
+
+    function nnn()
+
+    {
+        $name = "<script> var nam = prompt('Please Enter your Name .....')</script>";
+        $brand = "<script> prompt(parseInt('Please Enter Phone Brand Name....'))</script>";
+        $price = "<script> prompt(parseInt('Please Enter Price Of phone'))</script>";
+        $relashonship = "<script> prompt(parseInt('R You Married ?????'))</script>";
+
+        if ($name == "am") {
+            echo "<script> alert ('Welldone !!! .....')</script>";
+        }
     }
-else{
-echo"error";
-}
 
-$c=-2;
 
-if($c<0){
-    echo $c."<h1>This Is Negative </h1>";
-}
-elseif($c>0){
-    echo $c."<h1>This Is Positve </h1>";
-}
-else{
-    echo"error";
-}
+    // else {
+    //     echo "<script> alert ('Next Time !!! .....')</script>";
+    // }
 
-$d="admin";
-$e="123";
+    // echo $name, $brand, $price, $relashonship;
+    // if ($brand == "apple" and $price >= 1000 and $relashonship == "married") {
+    //     echo "<script>alert('Thankyou For Your Response')</>";
+    // } else {
+    //     echo "<script>alert('Please Enter Data')</>";
+    // }  //prompt function
 
-if($d=="admin" and $e=="123"){
-    echo $d."<h1> Wellcome Admin </h1>";
-}
-elseif($d!="admin" and $e!="123"){
-    echo $d."<h1>Sorry Try Again </h1>";
-}
-else{
-    echo"error";
-}
 
-$iphone=200000;
-$android=100000;
-$tech=40000;
 
-?>
+
+    ?>
+
+
+</body>
+
+</html>
